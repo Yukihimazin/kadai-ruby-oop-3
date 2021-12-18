34105925ab9ec78ad4f8f7fbd77917f50dea11f6
Yuki Takagi <gdhvtvibg@gmail.com>
2021-12-18 07:35:56 +0000

    kadai-ruby-oop-3

---
+++ human.rb
@@ -0,0 +1,16 @@
+require './animal'
+require './thinkable'
+
+class Human<Animal
+  
+  attr_accessor :hoby
+  
+  include Thinkable
+  
+  def initialize(name,age,hoby)
+    self.name=name
+    self.age=age
+    self.hoby=hoby
+  end
+  
+end
